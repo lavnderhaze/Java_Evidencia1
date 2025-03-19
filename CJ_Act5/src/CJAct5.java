93d495ced6ce711465af54e945293e35e34b8221
Daniela <danielacb2013@gmail.com>
2025-03-19 18:12:29 -0500

    Agregando Actividad 5, programa con distintas operaciones ariméticas

---
+++ CJ_Act5/src/CJAct5.java
@@ -0,0 +1,25 @@
+public class CJAct5 {
+    public static void main(String[] args) {
+          
+      // variables con valores  
+      double x1 = 13;
+      double x2 = 5;
+      double x3 = 29;
+  
+      // operaciones indicadas en la actividad
+      double Op1 = x1 + x2 - x3;
+      double Op2 = x2 - x3 * x1;
+      double Op3 = x1 * x2 * x3;
+      double Op4 = x3 / x1 + x2;
+  
+      // impresiones finales 
+      System.out.println("Primer valor = "+ x1);
+      System.out.println("Segundo valor = "+ x2);
+      System.out.println("Tercer valor = "+ x3);
+  
+      System.out.println("Primera operación = "+ Op1);
+      System.out.println("Segunda operación = "+ Op2);
+      System.out.println("Tercera operación = "+ Op3);
+      System.out.println("Cuarta operación = "+ Op4);
+      }
+  }
